ab0915cfa12b2544de5fc5da94f8b4f46d5024a8
harpreet kaur <harpreetvirdi17594@gmail.com>
2023-08-05 18:53:21 +0100

    updated code.js code added to change color

--- js/code.js
+++ js/code.js
@@ -5,9 +5,12 @@ var getInput = $("#getInput");
 currentDay.text(dayjs().format("dddd, MMMM D") + "th");
 var eventSaved = $("#eventSaved");
 
+var eventAdded = {
+  eventName: getInput.val().trim(),
+};
 //save button code
 $("#saveBtn").on("click", function () {
-  if (getInput === "") {
+  if (eventAdded.eventName === "") {
     alert("First name cannot be blank to save appointment");
   } else {
     alert("Appointment Added to local Storage");
@@ -18,9 +21,28 @@ $("#saveBtn").on("click", function () {
     // get most recent submission
     var enteredInputValue = JSON.parse(localStorage.getItem("getInput"));
     getInput.text(enteredInputValue);
-    getInput.attr("disabled", "disabled");
+    // getInput.attr("disabled", "disabled");
     eventSaved.text("Appointment Added to local Storage");
+    console.log(eventAdded);
   }
 });
 
-// ---------------------
+// setting color
+// =================================================================
+
+var systemTime = dayjs().format("h");
+var g = $("#getPTime");
+var getPTime = g.text();
+
+console.log(typeof getPTime);
+console.log(typeof systemTime);
+
+if (getPTime === systemTime) {
+  getInput.attr("style", "background-color:#ff6961;width: 100%; height: 50px");
+}
+if (getPTime > systemTime) {
+  getInput.attr("style", "background-color:#ff6961;width: 100%; height: 50px");
+}
+if (getPTime < systemTime) {
+  getInput.attr("style", "background-color:#ff6961;width: 100%; height: 50px");
+}
